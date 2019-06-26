8189864f7b91d9b7cb1564714b2c6f24f4ee91b3
Dan Ghadge <danghad@amazon.com>
2019-06-26 12:07:29 -0400

    refactored

--- src/main/java/healthcare/ez/model/Providers.java
+++ src/main/java/healthcare/ez/model/Providers.java
@@ -1,5 +1,265 @@
 package healthcare.ez.model;
 
 public class Providers {
+	private char last_name;
+	private char first_name;
+	private char middle_name;
+	private char suffix;
+	private String public_email;
+	private String web_site;
+	private char pp_prac_name;
+	private char pp_address1;
+	private char pp_address2;
+	private char pp_city;
+	private String pp_state;
+	private String pp_zip;
+	private String pp_telephone;
+	private char pp_language_office;
+	private char pp_language_practitioner;
+	private int pp_percent_location;
+	private String pp_days_seen;
+	private String board_eligible;
+	private String self_spec_code;
+	private int years_in_us_practice;
+	private int years_out_us_practice;
+	private String hospital_affiliated;
+	private String hospital_state;
+	private String continue_date;
+	private String lic_status;
+	private char grad_school;
+	private char grad_school_state;
+	private char grad_school_country;
+	private String grad_completion;
 
+	public char getLastName(){
+		return last_name;
+	}
+
+	public void setLastName(char last_name){
+		this.last_name=last_name;
+	}
+
+	public char getFirstName(){
+		return first_name;
+	}
+
+	public void setFirstName(char first_name){
+		this.first_name=first_name;
+	}
+
+	public char getMiddleName(){
+		return middle_name;
+	}
+
+	public void setMiddleName(char middle_name){
+		this.middle_name=middle_name;
+	}
+
+	public char getSuffix(){
+		return suffix;
+	}
+
+	public void setSuffix(char suffix){
+		this.suffix=suffix;
+	}
+
+	public String getPublicEmail(){
+		return public_email;
+	}
+
+	public void setPublicEmail(String public_email){
+		this.public_email=public_email;
+	}
+
+	public String getWebSite(){
+		return web_site;
+	}
+
+	public void setWebSite(String web_site){
+		this.web_site=web_site;
+	}
+
+	public char getPpPracName(){
+		return pp_prac_name;
+	}
+
+	public void setPpPracName(char pp_prac_name){
+		this.pp_prac_name=pp_prac_name;
+	}
+
+	public char getPpAddress1(){
+		return pp_address1;
+	}
+
+	public void setPpAddress1(char pp_address1){
+		this.pp_address1=pp_address1;
+	}
+
+	public char getPpAddress2(){
+		return pp_address2;
+	}
+
+	public void setPpAddress2(char pp_address2){
+		this.pp_address2=pp_address2;
+	}
+
+	public char getPpCity(){
+		return pp_city;
+	}
+
+	public void setPpCity(char pp_city){
+		this.pp_city=pp_city;
+	}
+
+	public String getPpState(){
+		return pp_state;
+	}
+
+	public void setPpState(String pp_state){
+		this.pp_state=pp_state;
+	}
+
+	public String getPpZip(){
+		return pp_zip;
+	}
+
+	public void setPpZip(String pp_zip){
+		this.pp_zip=pp_zip;
+	}
+
+	public String getPpTelephone(){
+		return pp_telephone;
+	}
+
+	public void setPpTelephone(String pp_telephone){
+		this.pp_telephone=pp_telephone;
+	}
+
+	public char getPpLanguageOffice(){
+		return pp_language_office;
+	}
+
+	public void setPpLanguageOffice(char pp_language_office){
+		this.pp_language_office=pp_language_office;
+	}
+
+	public char getPpLanguagePractitioner(){
+		return pp_language_practitioner;
+	}
+
+	public void setPpLanguagePractitioner(char pp_language_practitioner){
+		this.pp_language_practitioner=pp_language_practitioner;
+	}
+
+	public int getPpPercentLocation(){
+		return pp_percent_location;
+	}
+
+	public void setPpPercentLocation(int pp_percent_location){
+		this.pp_percent_location=pp_percent_location;
+	}
+
+	public String getPpDaysSeen(){
+		return pp_days_seen;
+	}
+
+	public void setPpDaysSeen(String pp_days_seen){
+		this.pp_days_seen=pp_days_seen;
+	}
+
+	public String getBoardEligible(){
+		return board_eligible;
+	}
+
+	public void setBoardEligible(String board_eligible){
+		this.board_eligible=board_eligible;
+	}
+
+	public String getSelf_spec_code(){
+		return self_spec_code;
+	}
+
+	public void setSelfSpecCode(String self_spec_code){
+		this.self_spec_code=self_spec_code;
+	}
+
+	public int getYearsInUsPractice(){
+		return years_in_us_practice;
+	}
+
+	public void setYearsInUsPractice(int years_in_us_practice){
+		this.years_in_us_practice=years_in_us_practice;
+	}
+
+	public int getYearsOutUsPractice(){
+		return years_out_us_practice;
+	}
+
+	public void setYearsOutUsPractice(int years_out_us_practice){
+		this.years_out_us_practice=years_out_us_practice;
+	}
+
+	public String getHospitalAffiliated(){
+		return hospital_affiliated;
+	}
+
+	public void setHospitalAffiliated(String hospital_affiliated){
+		this.hospital_affiliated=hospital_affiliated;
+	}
+
+	public String getHospitalState(){
+		return hospital_state;
+	}
+
+	public void setHospitalState(String hospital_state){
+		this.hospital_state=hospital_state;
+	}
+
+	public String getContinueDate(){
+		return continue_date;
+	}
+
+	public void setContinueDate(String continue_date){
+		this.continue_date=continue_date;
+	}
+
+	public String getLicStatus(){
+		return lic_status;
+	}
+
+	public void setLicStatus(String lic_status){
+		this.lic_status=lic_status;
+	}
+
+	public char getGradSchool(){
+		return grad_school;
+	}
+
+	public void setGradSchool(char grad_school){
+		this.grad_school=grad_school;
+	}
+
+	public char getGradSchoolState(){
+		return grad_school_state;
+	}
+
+	public void setGradSchoolState(char grad_school_state){
+		this.grad_school_state=grad_school_state;
+	}
+
+	public char getGradSchoolCountry(){
+		return grad_school_country;
+	}
+
+	public void setGradSchoolCountry(char grad_school_country){
+		this.grad_school_country=grad_school_country;
+	}
+
+	public String getGradCompletion(){
+		return grad_completion;
+	}
+
+	public void setGradCompletion(String grad_completion){
+		this.grad_completion=grad_completion;
+	}
 }
